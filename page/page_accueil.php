0e7f07ec2975d400760af8adec7814abae3c5c4e
samuel60378 <sam.holmes@live.fr>
2019-02-15 21:54:47 +0100

    Update page_accueil.php
    infobulle sur les images liens

--- page/page_accueil.php
+++ page/page_accueil.php
@@ -30,15 +30,15 @@
 			</div>
 			<div id ="footer">
 				<figure id="image">
-					<a href="page_contact.php"><img  src = "img/contact.png" height="50"/></a>
+					<a href="page_contact.php"><img  src = "img/contact.png" height="50" title="Contact"/></a>
 					<figcaption>Contact</figcaption>
 				</figure>
 				<figure id="image">
-					<a href="page_forum.php"><img src = "img/forum.png" height="45"/></a>
+					<a href="page_forum.php"><img src = "img/forum.png" height="45" title="Forum"/></a>
 					<figcaption>Forum</figcaption>
 				</figure>
 				<figure id="image">
-					<a href="page_boutique.php"><img src = "img/boutique.png" height="50"/></a>
+					<a href="page_boutique.php"><img src = "img/boutique.png" height="50" title="Boutique"/></a>
 					<figcaption>Boutique</figcaption>
 				</figure>
 				
